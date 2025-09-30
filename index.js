14f4be7a90c9be903d1f686e17753ad09ca8995b
Branice Simaloi <simaloibranice@gmail.com>
2025-10-01 00:43:35 +0300

    Complete filter functions

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+
+function findMatching(drivers, string) {
+  return drivers.filter(driver => driver.toLowerCase() === string.toLowerCase());
+}
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(driver => driver.toLowerCase().startsWith(string.toLowerCase()));
+}
+
+function matchName(drivers, string) {
+  return drivers.filter(driver => driver.name === string);
+}
